0a1e32149b03e6bed48aa97e62b8d42a910dc50b
Oskar Wrotniak <oskarwro@gmail.com>
2019-07-05 20:08:26 +0200

    Add playerMove 2 and 3

--- js/script.js
+++ js/script.js
@@ -1,11 +1,17 @@
-function printMessage(msg){
-	var div = document.createElement('div');
-	div.innerHTML = msg;
-	document.getElementById('messages').appendChild(div);
+let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
+
+console.log('Gracz wpisał: ' + playerInput);
+
+let playerMove = 'nieznany ruch';
+
+if(playerInput == '1'){
+  playerMove = 'kamień';
+}
+else if(playerInput == '2'){
+	playerMove = 'papier'
+}
+else(playerInput == '3'){
+	playerMove = 'nożyce'
 }
 
-function clearMessages(){
-	document.getElementById('messages').innerHTML = '';
-}
-
-let playerMove = 'papier';
+printMessage('Twój ruch to: ' + playerMove);
