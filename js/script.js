639a0324597f8f57d9ed87ba5a1c8b22482b2fd0
Oskar Wrotniak <oskarwro@gmail.com>
2019-07-09 17:18:07 +0200

    Add Game Results

--- js/script.js
+++ js/script.js
@@ -7,9 +7,9 @@ let computerMove = 'nieznany ruch';
 if (randomNumber == 1) {
     computerMove = 'kamień';
 } else if (randomNumber == 2) {
-    computerMove = 'papier'
+    computerMove = 'papier';
 } else if (randomNumber == 3) {
-    computerMove = 'nożyce'
+    computerMove = 'nożyce';
 }
 
 printMessage('Mój ruch to: ' + computerMove);
@@ -23,10 +23,37 @@ let playerMove = 'nieznany ruch';
 
 if (playerInput == '1') {
     playerMove = 'kamień';
-} else if (playerInput = '2') {
-    playerMove = 'papier'
-} else if (playerMove == '3') {
-    playerMove = 'nożyce'
+} else if (playerInput == '2') {
+    playerMove = 'papier';
+} else if (playerInput == '3') {
+    playerMove = 'nożyce';
+} else {
+    playerMove = 'nieznany ruch';
 }
 
 printMessage('Twój ruch to: ' + playerMove);
+
+
+if (computerMove == 'kamień' && playerMove == 'papier') {
+    printMessage('Ty wygrywasz!');
+} else if (computerMove == 'kamień' && playerMove == 'kamień') {
+    printMessage('Remis!');
+} else if (computerMove == 'kamień' && playerMove == 'nożyce') {
+    printMessage('Przegrywasz!');
+}
+
+if (computerMove == 'papier' && playerMove == 'papier') {
+    printMessage('Remis!');
+} else if (computerMove == 'papier' && playerMove == 'kamień') {
+    printMessage('Przegrywasz');
+} else if (computerMove == 'papier' && playerMove == 'nożyce') {
+    printMessage('Wygrywasz!');
+}
+
+if (computerMove == 'nożyce' && playerMove == 'papier') {
+    printMessage('Przegrywasz!');
+} else if (computerMove == 'nożyce' && playerMove == 'kamień') {
+    printMessage('Wygrywasz!');
+} else if (computerMove == 'nożyce' && playerMove == 'nożyce') {
+    printMessage('Remis!');
+}
